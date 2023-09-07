4610c37a32710d07d095b55027e8052370eed6ec
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-09-07 21:03:17 +0530

    Added comments

--- phase1/part1/factorial.s
+++ phase1/part1/factorial.s
@@ -5,17 +5,17 @@ number: .word 5
 
 main:
 
-lw a0,number
-li a1,1
-li a2,1
-li a3,0
+lw a0,number # load number from memory into a0
+li a1,1 # load 1 into a1
+li a2,1 # load 1 in a2
+li a3,0 # load 0 in a3
 
 loop:
 
-mul a2,a2,a1
-beq a1,a0,done
-addi a1,a1,1
-j loop
+mul a2,a2,a1 # a2 = a2 * a1
+beq a1,a0,done # if a1 == a0, branch to done
+addi a1,a1,1 # a1 = a1 + 1
+j loop # branch to loop
 
 done:
-sw a2,0(a3)
+sw a2,0(a3) # store result at memory location 0x00000000
