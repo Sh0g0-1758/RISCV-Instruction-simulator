8a242c040ef5737b1c8807375a252975210beb52
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-09-08 14:38:48 +0530

    Wrote the standard instructions for gcd_lcm

--- phase1/part1/gcd_lcm.s
+++ phase1/part1/gcd_lcm.s
@@ -6,19 +6,22 @@ num2: .word 8
 
 main:
 
-lw a0,num1 # load num1 from memory
-lw a1,num2 # load num2 from memory
-mv a4,a0 # a4 = a0
-mv a5,a1 # a5 = a1
+lui x23,0x10000
+lw a0,0(x23) # load num1 into x1
+lui x24,0x10000
+ori x24,x24,4
+lw a1,0(x24) # load num2 into x2
+addi a4,a0,0 # a4 = a0 + 0
+addi a5,a1,0 # a5 = a1 + 0
 
 # using The Euclidean algorithm to find the gcd of two numbers
 gcd_loop:
 
-beqz a1,gcd_done # if a1 == 0, branch to gcd_done
-mv a3,a1 # a3 = a1
+beq a1,x0,20 # if a1 == 0, branch to gcd_done
+addi a3,a1,0 # a3 = a1
 rem a1,a0,a1 # a1 = a0 % a1
-mv a0,a3 # a0 = a3
-j gcd_loop # jump to gcd_loop
+addi a0,a3,0 # a0 = a3
+jal x0,-16 # jump to gcd_loop
 
 # now simply using lcm = (a*b) / gcd
 gcd_done:
@@ -26,7 +29,7 @@ gcd_done:
 sw a0,0(x0) # storing a0 at memory address 0x00000000
 mul a3,a4,a5 # a3 = a4 * a5
 div a1,a3,a0 # a1 = a3 / a0
-j lcm_done
+jal x0,4
 
 lcm_done:
 
